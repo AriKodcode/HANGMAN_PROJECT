a95e57525bed5cb99af20fc9673269a3d4988b6a
Ari Durlacher <arikodcode@gmail.com>
2025-10-27 17:56:05 +0200

    fixed tests file

--- data/tests.py
+++ data/tests.py
@@ -3,16 +3,3 @@ data = ["banana", "apple", "eggplant", "tomato", "cucumber", "computer", "mouse"
 ,"bus", "car", "plane", "boat", "bicycle", "train", "road", "map", "city", "village"]
 
 
-# ,"Desert", "Forest", "Sea", "Mountain", "Snow", "Rain", "Sun", "Moon", "Star", "Sky"
-# ,"Shirt", "Pants", "Shoes", "Socks", "Coat", "Hat", "Scarf", "Umbrella", "Watch", "Glasses"
-# ,"Cat", "Dog", "Fish", "Bird", "Horse", "Sheep", "Goat", "Cow", "Chicken", "Duck"
-# ,"Tiger", "Elephant", "Monkey", "Lion", "Bear", "Camel", "Fox", "Wolf", "Jackal", "Owl"
-# ,"Flower", "Leaf", "Tree", "Bush", "Grass", "Stone", "River", "Lake", "Waterfalls", "Campfire"
-# ,"Water", "Sand", "Earth", "Sky", "Wind", "Lightning", "Cloud", "Storm", "Rainbow", "Puddle"
-# ,"Doctor", "Teacher", "Driver", "Firefighter", "Policeman", "Soldier", "Cook", "Farmer", "Baker", "Cleaner"
-# ,"Painter", "Musician", "Carpenter", "Electrician", "Scientist", "Writer", "Engineer", "Photographer", "Student", "Coach"
-# ,"Happiness", "Sadness", "Fear", "Love", "Anger", "Excitement", "Boredom", "Hope", "Worry", "Peace"
-# ,"Bread", "Cheese", "Egg", "Butter", "Chocolate", "Cake", "Ice Cream", "Coffee", "Tea", "pizza"
-# ,"sugar", "salt", "pepper", "onion", "garlic", "flour", "rice", "soup", "milk", "chicken"
-# ,"ship", "car", "motorcycle", "helicopter", "cargo", "port", "station", "road", "interchange", "tunnel"
-# "house", "building", "stairs", "floor", "roof", "room", "kitchen", "bathroom", "toilet", "living room"
